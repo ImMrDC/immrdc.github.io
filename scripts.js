db910c5b9a184fa2211c00966407c57af0fe2cd5
ImMrDC <dc@imdc.io>
2025-08-09 15:24:29 +0100

    new layout - header

--- scripts.js
+++ scripts.js
@@ -2,7 +2,14 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     // Vanilla JS to insert email    
     const email = document.querySelector('.load-email');
-    email.innerHTML = `<a href="mailto:${email.dataset.u}@${email.dataset.d}">${email.dataset.u}@${email.dataset.d}</a>`;
+    const link = document.createElement('a');
+link.href = `mailto:${email.dataset.u}@${email.dataset.d}`;
+
+// Move existing content inside the <a>
+link.append(...email.childNodes);
+
+// Replace the original content with the link
+email.appendChild(link);
 
     // const phone = document.querySelector('.load-phone');
     // phone.innerHTML = `<a href="tel:${phone.dataset.a}${phone.dataset.b}">${phone.dataset.c}${phone.dataset.d}</a>`;
